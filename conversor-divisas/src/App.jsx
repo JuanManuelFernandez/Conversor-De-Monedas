b7811b55f3521c295dc0b6e39e725ee65e9b34d5
JuanManuelFernandez <juanmanuelfernandezbogo@gmail.com>
2026-07-23 16:28:33 -0300

    Se agrego un formato distinto a la selección de divisas con sus banderas

--- conversor-divisas/src/App.jsx
+++ conversor-divisas/src/App.jsx
@@ -1,20 +1,35 @@
 import { useState } from 'react'
+import Select from 'react-select'
 import './App.css'
 
+const opcionesMoneda = [
+  {value: 'USD', label: 'USD', bandera: '/src/assets/banderas/EEUU.png'},
+  {value: 'EUR', label: 'EUR', bandera: '/src/assets/banderas/Esp.png' },
+  {value: 'ARS', label: 'ARS', bandera: '/src/assets/banderas/Arg.png' },
+]
+
 function App() {
-  const [actual, setActual] = useState('USD'); //Moneda de origen
-  const [equivalente, setEquivalente] = useState('EUR'); //Moneda destino
+  const [actual, setActual] = useState(opcionesMoneda[0]); //Moneda de origen
+  const [equivalente, setEquivalente] = useState(opcionesMoneda[2]); //Moneda destino
   const [cantidad, setCantidad] = useState(1); //Guarda el monto que se quiere convertir
   const [resultado, setResultado] = useState(null); //Guarda el resultado final convertido
+  
+  const formatoOpcion = (opcion) => (
+    <div style={{ display: 'flex', alignItems: 'center', gap: '8px'}}>
+      <img src={opcion.bandera} alt={opcion.value} style={{ width: '20px' }} />
+      <span>{opcion.label}</span>
+    </div>
+  )
 
   const convertir = async () => {
     try{ 
       console.log(actual)
-      const res = await fetch(`https://open.er-api.com/v6/latest/${actual}`)
+      const res = await fetch(`https://open.er-api.com/v6/latest/${actual.value}`)
       const data = await res.json();
       console.log(data);
-      const rate = data.rates[equivalente];
+      const rate = data.rates[equivalente.value];
       setResultado((cantidad * rate).toFixed(2))
+      console.log(resultado)
     }
     catch(error){
       console.log("Error en la conversión");
@@ -32,19 +47,23 @@ function App() {
           <div className='BoxIzq'>
             <div className='ContenedorTipo'>
               <div className='ContenedorActual'>
-                <select className='SelectTipo' value={actual} onChange={e => setActual(e.target.value)}>
-                  <option value='USD'>USD</option>
-                  <option value='EUR'>EUR</option>
-                  <option value='ARS'>ARS</option>
-                </select>
+                <Select
+                  options={opcionesMoneda}
+                  value={actual}
+                  onChange={(opcionSeleccionada) => setActual(opcionSeleccionada)}
+                  formatOptionLabel={formatoOpcion}
+                  className='SelectTipo'
+                />
                 <input className='InputMonto' type='number' value={cantidad} onChange={e => setCantidad(e.target.value)}></input>
               </div>
 
-              <select className='SelectEquivalente' value={equivalente} onChange={e => setEquivalente(e.target.value)}>
-                  <option value='USD'>USD</option>
-                  <option value='EUR'>EUR</option>
-                  <option value='ARS'>ARS</option>
-              </select>
+              <Select
+                options={opcionesMoneda}
+                value={equivalente}
+                onChange={(opcionEquivalente) => setEquivalente(opcionEquivalente)}
+                formatOptionLabel={formatoOpcion}
+                className='SelectEquivalente'
+              />
             </div>
             <button className='BtnConvertir' onClick={convertir}>Convertir<img src='src/assets/Flecha.png' className='IconoFlecha'></img></button>
           </div>
